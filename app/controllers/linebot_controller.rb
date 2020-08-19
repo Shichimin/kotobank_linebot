f9d1f2c2b3921d16d1b1ecfa1c7ddfa749c67a06
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-20 08:33:21 +0900

    デバッグ

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,5 +1,15 @@
 class LinebotController < ApplicationController
-  require 'mechanize'
+  require 'line/bot'
+
+  # callbackアクションのCSRFトークン認証を無効
+  protect_from_forgery :except => [:callback]
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
 
   def callback
     body = request.body.read
